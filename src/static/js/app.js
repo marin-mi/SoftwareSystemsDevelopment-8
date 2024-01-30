b93ad3897f605f0faf1f4d054de0aef8dd9c7cdd
marin-mi <marin0109yuki@gmail.com>
2024-01-30 22:39:45 +0900

    Add title

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,6 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
+                    <h1 style={{ textAlign: 'center' }}>Marin Mikami(marin-mi)</h1>
                     <TodoListCard />
                 </Col>
             </Row>
